93cf7431ce1988df21cd1861f02ba3ce7cf665ab
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2023-03-03 17:29:52 +0300

    Update plot.sql

--- firstLab/plot.sql
+++ firstLab/plot.sql
@@ -12,7 +12,8 @@ CREATE TABLE humans (
 
 CREATE TABLE manipulators (
                               id bigint primary key,
-                              name varchar unique
+                              name varchar unique,
+                              type varchar
 );
 
 CREATE TABLE states (
@@ -27,3 +28,21 @@ CREATE TABLE operator (
                             manipulator_ID INTEGER REFERENCES manipulators,
                             PRIMARY KEY (humans_ID, manipulator_ID)
 );
+
+INSERT INTO characteristics (id, value) values
+                            (1, 'умелый'),
+                            (2, 'метровая труба, заканчивающаяся башмаком, вроде тех, на которые приземляются межпланетные аппараты'),
+                            (3, 'раздвигается в несколько раз, похож на помело и непростой в обращении');
+                            
+INSERT INTO humans (id, name, surname, characteristic_id)  values
+                   (1, 'Letchik', 'Pilotov', 1);
+                   
+INSERT INTO manipulators (id, name, type)  values
+                         (1, 'MP0432', 'универсальный');
+                         
+INSERT INTO states (id, value, characteristic_id, manipulator_id)  values
+                   (1, 'сложенный', 2, 1),
+                   (2, 'разложенный', 3, 1);
+                  
+INSERT INTO operator (humans_id, manipulator_id)  values
+                     (1, 1);
