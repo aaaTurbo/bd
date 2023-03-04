8a1b5f0d1f7142f7104f16cbf1f7debf901affb6
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2023-03-04 12:12:17 +0300

    Update plot.sql

--- firstLab/plot.sql
+++ firstLab/plot.sql
@@ -1,53 +1,58 @@
-CREATE TABLE characteristics (
-                         id     serial primary key,
-                         value  varchar unique
+CREATE TABLE characteristics
+(
+    id    serial primary key,
+    value varchar unique
 );
 
-CREATE TABLE humans (
-                        id     serial primary key,
-                        name  varchar,
-                        surname varchar,
-                        characteristic_ID bigint REFERENCES characteristics
+CREATE TABLE humans
+(
+    id      serial primary key,
+    name    varchar,
+    surname varchar
 );
 
-CREATE TABLE manipulators (
-                              id serial primary key,
-                              name varchar unique,
-                              type varchar
+CREATE TABLE manipulators
+(
+    id   serial primary key,
+    name varchar unique,
+    type varchar
 );
 
-CREATE TABLE states (
-                              id serial primary key,
-                              value varchar unique,
-                              characteristic_ID bigint REFERENCES characteristics,
-                              manipulator_ID bigint REFERENCES manipulators
+CREATE TABLE states
+(
+    id                serial primary key,
+    value             varchar unique,
+    characteristic_ID bigint REFERENCES characteristics,
+    manipulator_ID    bigint REFERENCES manipulators
 );
 
-CREATE TABLE operator (
-                            humans_ID  INTEGER REFERENCES humans,
-                            manipulator_ID INTEGER REFERENCES manipulators,
-                            PRIMARY KEY (humans_ID, manipulator_ID)
+CREATE TABLE operator
+(
+    humans_ID      INTEGER REFERENCES humans,
+    manipulator_ID INTEGER REFERENCES manipulators,
+    PRIMARY KEY (humans_ID, manipulator_ID)
 );
 
-INSERT INTO characteristics (value) values
-                                          ('метровая труба, заканчивающаяся башмаком, вроде тех, на которые приземляются межпланетные аппараты'),
-                                          ('раздвигается в несколько раз, похож на помело и непростой в обращении');
+INSERT INTO characteristics (value)
+values ('метровая труба, заканчивающаяся башмаком, вроде тех, на которые приземляются межпланетные аппараты'),
+       ('раздвигается в несколько раз, похож на помело и непростой в обращении');
 
-INSERT INTO humans (name, surname, characteristic_id)  values
-                   ('Letchik', 'Pilotov', 1);
+INSERT INTO humans (name, surname)
+values ('Letchik', 'Pilotov');
 
-INSERT INTO manipulators (name, type)  values
-    ('MP0432', 'универсальный');
+INSERT INTO manipulators (name, type)
+values ('MP0432', 'универсальный');
 
-INSERT INTO states (value, characteristic_id, manipulator_id)  values
-    ('сложенный', 1, 1),
-    ('разложенный', 2, 1);
+INSERT INTO states (value, characteristic_id, manipulator_id)
+values ('сложенный', 1, 1),
+       ('разложенный', 2, 1);
 
-INSERT INTO operator (humans_id, manipulator_id)  values
-                     (1, 1);
+INSERT INTO operator (humans_id, manipulator_id)
+values (1, 1);
 
-SELECT human.name, human.surname, manipulator.name, state.value, characteristic.value FROM operator
-    INNER JOIN manipulators manipulator on operator.manipulator_id = manipulator.id
-    INNER JOIN humans human on operator.humans_id = human.id
-    INNER JOIN states state on operator.manipulator_id = state.manipulator_id
-    INNER JOIN characteristics characteristic on state.characteristic_id = characteristic.id
+SELECT human.name, human.surname, manipulator.name, state.value, characteristic.value
+FROM operator
+         INNER JOIN manipulators manipulator on operator.manipulator_id = manipulator.id
+         INNER JOIN humans human on operator.humans_id = human.id
+         INNER JOIN states state on operator.manipulator_id = state.manipulator_id
+         INNER JOIN characteristics characteristic on state.characteristic_id = characteristic.id
