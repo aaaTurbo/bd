37f82ed5ca38db2ffd101b9deb834bd017437611
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2023-03-04 12:08:21 +0300

    Update plot.sql

--- firstLab/plot.sql
+++ firstLab/plot.sql
@@ -1,5 +1,5 @@
 CREATE TABLE characteristics (
-                         id     bigint primary key,
+                         id     serial primary key,
                          value  varchar unique
 );
 
@@ -11,13 +11,13 @@ CREATE TABLE humans (
 );
 
 CREATE TABLE manipulators (
-                              id bigint primary key,
+                              id serial primary key,
                               name varchar unique,
                               type varchar
 );
 
 CREATE TABLE states (
-                              id bigint primary key,
+                              id serial primary key,
                               value varchar unique,
                               characteristic_ID bigint REFERENCES characteristics,
                               manipulator_ID bigint REFERENCES manipulators
@@ -29,20 +29,25 @@ CREATE TABLE operator (
                             PRIMARY KEY (humans_ID, manipulator_ID)
 );
 
-INSERT INTO characteristics (id, value) values
-                            (1, 'умелый'),
-                            (2, 'метровая труба, заканчивающаяся башмаком, вроде тех, на которые приземляются межпланетные аппараты'),
-                            (3, 'раздвигается в несколько раз, похож на помело и непростой в обращении');
-                            
+INSERT INTO characteristics (value) values
+                                          ('метровая труба, заканчивающаяся башмаком, вроде тех, на которые приземляются межпланетные аппараты'),
+                                          ('раздвигается в несколько раз, похож на помело и непростой в обращении');
+
 INSERT INTO humans (name, surname, characteristic_id)  values
                    ('Letchik', 'Pilotov', 1);
-                   
-INSERT INTO manipulators (id, name, type)  values
-                         (1, 'MP0432', 'универсальный');
-                         
-INSERT INTO states (id, value, characteristic_id, manipulator_id)  values
-                   (1, 'сложенный', 2, 1),
-                   (2, 'разложенный', 3, 1);
-                  
+
+INSERT INTO manipulators (name, type)  values
+    ('MP0432', 'универсальный');
+
+INSERT INTO states (value, characteristic_id, manipulator_id)  values
+    ('сложенный', 1, 1),
+    ('разложенный', 2, 1);
+
 INSERT INTO operator (humans_id, manipulator_id)  values
                      (1, 1);
+
+SELECT human.name, human.surname, manipulator.name, state.value, characteristic.value FROM operator
+    INNER JOIN manipulators manipulator on operator.manipulator_id = manipulator.id
+    INNER JOIN humans human on operator.humans_id = human.id
+    INNER JOIN states state on operator.manipulator_id = state.manipulator_id
+    INNER JOIN characteristics characteristic on state.characteristic_id = characteristic.id
